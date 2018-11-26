5948738e39c9a60a405525844e95b4de427c170d
Cura <cura@hellodev.kr>
2018-11-26 16:28:35 +0900

    added

---
+++ src/main/java/kr/hellodev/support/commons/session/BaseSessionContext.java
@@ -0,0 +1,13 @@
+package kr.hellodev.support.commons.session;
+
+import java.io.Serializable;
+
+/**
+ * Project : commons-java
+ * Developer : acu77
+ * Date : 26/11/2018 4:28 PM
+ */
+public abstract class BaseSessionContext implements Serializable {
+
+    private static final long serialVersionUID = -3778527427247480823L;
+}
